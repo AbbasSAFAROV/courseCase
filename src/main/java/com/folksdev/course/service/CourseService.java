eac15358647a7867cf100cd7b50e183e574ce2e7
FIRST_NAME LAST_NAME <abbasabdullaziz@gmail.com>
2021-12-20 01:03:58 +0300

    courseDto fixed

--- src/main/java/com/folksdev/course/service/CourseService.java
+++ src/main/java/com/folksdev/course/service/CourseService.java
@@ -1,15 +1,37 @@
 package com.folksdev.course.service;
 
 
+import com.folksdev.course.configuration.Config;
+import com.folksdev.course.model.dto.CourseDto;
+import com.folksdev.course.model.request.CourseCreateRequest;
 import com.folksdev.course.repository.CourseRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CourseService {
 
     private final CourseRepository courseRepository;
+    private final Config config;
+    private final ModelMapper modelMapper;
 
-    public CourseService(CourseRepository courseRepository) {
+    public CourseService(CourseRepository courseRepository, Config config, ModelMapper modelMapper) {
         this.courseRepository = courseRepository;
+        this.config = config;
+        this.modelMapper = modelMapper;
     }
+
+    public List<CourseDto> getAllCourses(){
+        return courseRepository.findAll().stream().map(x->modelMapper.map(x,CourseDto.class)).collect(Collectors.toList());
+    }
+
+    public CourseDto createCourse(CourseCreateRequest courseCreateRequest){
+        return new CourseDto();
+    }
+
+
+
 }
