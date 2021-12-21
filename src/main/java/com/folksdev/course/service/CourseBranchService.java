14e65dbfa6e743e68808f647f827cec44e6eb820
FIRST_NAME LAST_NAME <abbasabdullaziz@gmail.com>
2021-12-22 02:30:22 +0300

    course branch service extended added some methods

--- src/main/java/com/folksdev/course/service/CourseBranchService.java
+++ src/main/java/com/folksdev/course/service/CourseBranchService.java
@@ -2,23 +2,39 @@ package com.folksdev.course.service;
 
 
 import com.folksdev.course.entity.CourseBranch;
+import com.folksdev.course.exception.CourseBranchNotFound;
 import com.folksdev.course.model.dto.CourseBranchDto;
 import com.folksdev.course.repository.CourseBranchRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CourseBranchService {
 
     private final CourseBranchRepository courseBranchRepository;
+    private final ModelMapper modelMapper;
 
-    public CourseBranchService(CourseBranchRepository courseBranchRepository) {
+    public CourseBranchService(CourseBranchRepository courseBranchRepository, ModelMapper modelMapper) {
         this.courseBranchRepository = courseBranchRepository;
+        this.modelMapper = modelMapper;
     }
 
     public List<CourseBranchDto> getAllCourseBranches(){
-        return null;
+        return courseBranchRepository.findAll().stream().map(x->modelMapper.map(x,CourseBranchDto.class)).collect(Collectors.toList());
+    }
+
+    public CourseBranchDto getCourseBranchById(Long id){
+        return modelMapper.map(findCourseById(id),CourseBranchDto.class);
+    }
+
+
+
+
+    public CourseBranch findCourseById(Long id){
+        return courseBranchRepository.findById(id).orElseThrow(()->new CourseBranchNotFound("Course branch not found with this id "+id));
     }
 
 }
