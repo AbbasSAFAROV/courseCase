a7184eb08deefb6a810d73e61bc9cdfaf84eb82b
FIRST_NAME LAST_NAME <abbasabdullaziz@gmail.com>
2021-12-21 18:25:17 +0300

    course oontroller added

---
+++ src/main/java/com/folksdev/course/api/CourseController.java
@@ -0,0 +1,43 @@
+package com.folksdev.course.api;
+
+
+import com.folksdev.course.entity.Course;
+import com.folksdev.course.model.dto.CourseDto;
+import com.folksdev.course.model.request.CourseCreateRequest;
+import com.folksdev.course.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseDto>> getAllCourses(){
+        return new ResponseEntity<>(courseService.getAllCourses(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseDto> getCourseById(@PathVariable Long id){
+        return new ResponseEntity<>(courseService.getCourseById(id),HttpStatus.OK)
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDto> createCourse(@RequestBody CourseCreateRequest createRequest){
+        return new ResponseEntity<>(courseService.createCourse(createRequest),HttpStatus.CREATED);
+    }
+
+
+
+
+
+}
